6b0caa2e9df401bfe87e2b02309c568c2bc139a1
noorfarooqy <noor@drongo.vip>
2024-03-22 20:36:00 +0300

    add colum is system

--- database/migrations/2024_03_21_190309_add_column_is_system_to_users_table.php
+++ database/migrations/2024_03_21_190309_add_column_is_system_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('is_system')->after('profile_photo_path')->default(false);
+            $table->boolean('is_system')->before('created_at')->default(false);
         });
     }
 
